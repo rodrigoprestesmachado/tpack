9985ad8246307df869e87d6704dc7f0e44f4a5c7
Rodrigo Prestes Machado <rodrigo@Mac-mini.localdomain>
2020-08-17 11:10:25 -0300

    interface

--- ui/src/components/Session.vue
+++ ui/src/components/Session.vue
@@ -1,9 +1,17 @@
 <template>
   <v-card flat color="transparent">
     {{session.title}}
-    <div v-for="item of session.questions" :key="item.id">
-      <v-subheader>{{item.text}}</v-subheader>
-      <v-slider thumb-label="always" :step="0.1" :max="5" :min="1" />
+    <div v-for="question of session.questions" :key="question.id">
+      <v-subheader>{{question.text}}</v-subheader>
+      <v-slider
+        :v-model="answer[question.id]"
+        thumb-label="always"
+        :step="0.1"
+        @change="onChange(question.id, $event)"
+        :max="5"
+        :min="1"
+        :value="answer[question.id]"
+      />
     </div>
     <div v-if="backButton">
       <v-btn small color="#D3EAE1" v-on:click="getSession(session.id - 1)">Voltar</v-btn>
@@ -18,10 +26,15 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Session extends Vue {
+  /** The base URL */
   private readonly BASE: string;
+  /** Stores the answer of the users to only sync with the interface */
+  private answer: any = [];
+  /** The session with the questions */
   private session: any = [];
+  /** controls the button used to back the sessions */
   private backButton: boolean;
-
+  
   constructor() {
     super();
     this.backButton = false;
@@ -29,17 +42,51 @@ export default class Session extends Vue {
     this.getFirstSession(this.BASE);
   }
 
+  /**
+   * Gets the first session from the service
+   * 
+   * @param base The URL base of the service
+   */
   getFirstSession(base: string) {
     const url = base + "getFirstSession";
-    axios.get(url).then(response => (this.session = response.data));
+    axios.get(url).then(response => {
+      this.session = response.data
+      this.loadFromLocalStorage();
+    });
   }
 
+  /**
+   * Gets the a spefific session from the service
+   * 
+   * @param id The identificator of the session
+   */
   getSession(id: string) {
-    console.log(id);
     axios.get(this.BASE + "getSession/" + id).then(response => {
       this.backButton = true;
       this.session = response.data;
     });
+  }
+
+  /**
+   * Registers the answer of the user
+   * 
+   * @param idQuestion The identificator of the question
+   * @param response  The answer of the user
+   */
+  onChange(idQuestion: string, response: string) {
+    // this.answer only controls the user's interface
+    this.answer[idQuestion] = response;
+    // store the answers in the local storage
+    localStorage.setItem(idQuestion, response);
+  }
+
+  /**
+   * Loads the data from local storage
+   */
+  loadFromLocalStorage() {
+    for (let i = 0; i < localStorage.length; i++) {
+      this.answer[i] = localStorage.getItem(i.toString());
+    }
   }
 }
 </script>
