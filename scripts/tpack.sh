8bba06b8204688223ab3f6250b87537f5acd149f
Rodrigo <rodrigo.prestes@gmail.dom>
2020-09-17 19:36:59 -0300

    #1

--- scripts/tpack.sh
+++ scripts/tpack.sh
@@ -7,11 +7,18 @@ cd ../ui
 npm install
 vue-cli-service build --mode production --dest ../src/main/webapp --target app --modern --dashboard
 
+echo "---------------------------------------------------"
+echo "2 - Copying settings.xml do ./m2 (Maven) directory"
+echo "---------------------------------------------------"
+cd ../scripts
+cp settings.xml $HOME/.m2/
+cd ..
+
 echo "----------------------------------------"
 echo "2 - Building microservice with Maven"
 echo "----------------------------------------"
 cd ..
-mvn clean package
+mvn clean replacer:replace package
 
 echo "----------------------------------------"
 echo "3 - Starting Docker Compose"
