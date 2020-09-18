a6a44d2f57bb400bd9f97f8ac70efe00ca745636
Rodrigo <rodrigo.prestes@gmail.dom>
2020-09-18 19:25:06 -0300

    #3

--- scripts/tpack.sh
+++ scripts/tpack.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 clear
 echo "----------------------------------"
-echo "1 - Building Tpack user interface"
+echo "1 - Configuring Tpack application"
 echo "----------------------------------"
+cp settings.xml $HOME/.m2/
+cd ..
 mvn replacer:replace
-cd ../ui
+
+echo "----------------------------------"
+echo "2 - Building Tpack Web interface"
+echo "----------------------------------"
+cd ui
 npm install
 npm run build --fix
 cp -a dist/* ../src/main/webapp
-
-echo "---------------------------------------------------"
-echo "2 - Copying settings.xml do ./m2 (Maven) directory"
-echo "---------------------------------------------------"
-cd ../scripts
-cp settings.xml $HOME/.m2/
-
-echo "---------------------------------"
-echo "3 - Building Tpack Microservice"
-echo "---------------------------------"
 cd ..
+
+echo "---------------------------------"
+echo "3 - Building Tpack microservice"
+echo "---------------------------------"
 mvn clean package
 
-echo "-----------------------------"
-echo "4 - Starting docker-compose"
-echo "-----------------------------"
+echo "-------------------------------"
+echo "4 - Starting Tpack application"
+echo "------------------------------"
 docker-compose up -d
