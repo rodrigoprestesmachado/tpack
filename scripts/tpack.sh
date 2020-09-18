51e8df7cfb65732d44a75d505eccb6ab4cdcd7fc
Rodrigo <rodrigo.prestes@gmail.dom>
2020-09-17 21:15:29 -0300

    #3

--- scripts/tpack.sh
+++ scripts/tpack.sh
@@ -5,14 +5,13 @@ echo "1 - Building Vue user interface with NPM"
 echo "----------------------------------------"
 cd ../ui
 npm install
-vue-cli-service build --mode production --dest ../src/main/webapp --target app --modern --dashboard
+npm run build
 
 echo "---------------------------------------------------"
 echo "2 - Copying settings.xml do ./m2 (Maven) directory"
 echo "---------------------------------------------------"
 cd ../scripts
 cp settings.xml $HOME/.m2/
-cd ..
 
 echo "----------------------------------------"
 echo "2 - Building microservice with Maven"
