9f1e922201b4c3e5456cc7ddb62702c82a296dd6
cleberramiresmachado <72587825+cleberramiresmachado@users.noreply.github.com>
2020-10-09 13:48:55 -0300

    Update CORSFilter.java

--- src/main/java/edu/ifrs/tpack/CORSFilter.java
+++ src/main/java/edu/ifrs/tpack/CORSFilter.java
@@ -3,8 +3,12 @@
  * Copyright 2020 TPACK XS Application
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
+*/
+/**
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
+ */
+/**
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +25,9 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
 @Provider
+/**
+* Teste com inserção de comentários
+*/
 public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
