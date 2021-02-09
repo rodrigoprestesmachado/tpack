a4f931f4f19a861eef265df6d0fc4e32f08450b8
Roger Baracy <rogerbaracy.s@gmail.com>
2021-02-09 00:31:10 -0300

    adicionado os comentarios de javadoc

--- src/main/java/edu/ifrs/tpack/CORSFilter.java
+++ src/main/java/edu/ifrs/tpack/CORSFilter.java
@@ -25,6 +25,12 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
+    /**
+     * 
+     * @param requestContext
+     * @param responseContext
+     * @throws IOException
+     */
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
