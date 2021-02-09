f02413624c6655b118c0041b99bec8440300820c
Vitor Rosa <58368306+VitorSdaRosa@users.noreply.github.com>
2021-02-09 02:11:19 -0300

    Update Subject.java

--- src/main/java/edu/ifrs/tpack/model/Subject.java
+++ src/main/java/edu/ifrs/tpack/model/Subject.java
@@ -1,19 +1,3 @@
-/**
- * @License
- * Copyright 2020 TPACK XS Application
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package edu.ifrs.tpack.model;
 
 import java.sql.Timestamp;
@@ -33,18 +17,33 @@ import lombok.Data;
 
 @Data
 @Entity
+/**
+ * Comentário da classe Subject
+ */
 public class Subject {
 
     @Id
     @GeneratedValue
     @JsonbTransient
+    /**
+     * Comentário 
+     */
     private long id;
 
+    /**
+     * Comentário 
+     */
     private String token;
 
+    /**
+     * Comentário 
+     */
     private Timestamp moment;
 
     @OneToMany(mappedBy = "subject", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    /**
+     * Comentário 
+     */
     private List<Answer> answers;
 
     /**
