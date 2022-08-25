11dbb5a089d0db7915136150f61b38cb96bd164a
Francisco Wolfarth Mossi <kikomossi@gmail.com>
2022-08-25 20:36:32 -0300

    fixing pmd errors on question class

--- src/main/java/edu/ifrs/tpack/model/Question.java
+++ src/main/java/edu/ifrs/tpack/model/Question.java
@@ -5,14 +5,6 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package edu.ifrs.tpack.model;
 
@@ -31,33 +23,57 @@ import javax.persistence.OneToMany;
 import javax.persistence.PostLoad;
 import javax.persistence.Transient;
 
+/**
+ * class represenst a question
+ */
 @Entity
 public class Question {
 
+    /**
+     * question identifier
+     */
     @Id
     @GeneratedValue
     private long id;
 
+    /**
+     * the question itself
+     */
     @Column(name = "TEXT", length = 1500)
     private String text;
 
-    /* one explanation about the question */
+    /**
+     * one explanation about the question
+     */
     private String note;
 
-    /* identify the type of the question */
+    /**
+     * identify the type of the question
+     */
     private QuestionType type;
 
-    /* indicates the number os levels in multilevel questions */
     @Transient
+    /**
+     * indicates the number os levels in multilevel questions
+     */
     private byte levels;
 
+    /**
+     * the session the question belongs
+     */
     @ManyToOne
     @JoinColumn(name = "SESSION_ID", nullable = false)
     private Session session;
 
+    /**
+     * question options
+     */
     @OneToMany(mappedBy = "question", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     private List<Choice> choices;
 
+    /**
+     * question awnsers
+     */
     @OneToMany(mappedBy = "question", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     @JsonbTransient
     private List<Answer> answers;
