dcb534439d45b90e93d64ad06025b878edbb0be2
Meridiane <meridiane@orderby.com.br>
2022-08-31 20:43:45 -0300

    pmd testes

--- src/main/java/edu/ifrs/tpack/data/DAOSession.java
+++ src/main/java/edu/ifrs/tpack/data/DAOSession.java
@@ -1,19 +1,4 @@
-/**
- * @License
- * Copyright 2020 TPACK XS Application
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package edu.ifrs.tpack.data;
 
 import javax.persistence.Query;
@@ -38,6 +23,7 @@ public class DAOSession extends Repository<Session> {
         // Order by id
         criteria.orderBy(builder.asc(root.get("id")));
         // Get the first register
+        /*COMENTÁRIO */
         Query query = em.createQuery(criteria).setMaxResults(1);
         return (Session) query.getResultList().get(0);
     }
